63de8f890a7e61ae98f30dc4eca5806163fbddd5
conrading <trafficking22@gmail.com>
2021-11-03 20:21:49 +0100

    building mainpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import './App.css';
 import MainPlayer from './component/mainPlayer'
-import Status from './component/status'
-import Chain from './component/chain'
-import Purchase from './component/purchase'
-import { Button, NavLink, InputGroup, Input, Container, Row, Col, Nav, NavItem } from 'reactstrap';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import MainPage from './component/mainPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
-class App extends Component {
+class Mainframe extends Component {
   constructor (props) {
     super (props)
     this.search = this.search.bind(this)
@@ -18,52 +16,22 @@ class App extends Component {
   render () {
     return (
       <Router>
-      <Container>
-        <div>
-          <div className="main-page">
-            <Row><p><h3>Silex | Oasis</h3></p></Row>
+      <div>
+        <div className="making-row separate-two-side">
+          <div className="title text text-pointer" onClick={() => {window.location = `/`}}>Chainmatic | 鏈 鎖 機 制</div>
+          <div className="making-row">
+            <div className="width-option text-pointer text" onClick={() => {window.location = '/hauptsachlich'}}>門</div>
+            <div className="width-option text-pointer text" onClick={() => {window.location = '/peoplesearch'}}>##</div>
           </div>
-          <Row>
-            <Col xs="7">
-              <Nav>
-                <NavItem>
-                  <Link to={'/'} className="nav-link">Main Page</Link>  
-                </NavItem>
-                <NavItem>
-                  <Link to={'/status'} className="nav-link">Status</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={'/purchase'} className="nav-link">Purchase</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={'/chain'} className="nav-link">Chain</Link>
-                </NavItem>
-                <NavItem>
-                  <NavLink>Conversation</NavLink>
-                </NavItem>
-              </Nav>
-            </Col>
-            <Col>
-              <InputGroup>
-                <Input placeholder="search" />
-              </InputGroup>
-            </Col>
-            <Col xs="2">
-              <Button color="link" onClick={this.search}>search</Button>
-            </Col>
-          </Row>
-          <hr />
-          <Switch>
-            <Route exact path='/' component={MainPlayer} />
-            <Route exact path='/status' component={Status} />
-            <Route exact path='/purchase' component={Purchase} />
-            <Route exact path='/chain' component={Chain} />
-          </Switch>
         </div>
-        <div className="main-page">
-          <h6>2020 Silex Oasis. All Rights Reserved</h6>
-        </div>
-      </Container>
+        <hr />
+        <Switch>
+          <Route exact path='/' component={MainPage} />
+        </Switch>
+      </div>
+      <div className="center-text">
+        <div>@2021 Chainmatic | 鏈 鎖 機 制, All Rights Reserved</div>
+      </div>
       </Router>
     );
   }
@@ -71,4 +39,4 @@ class App extends Component {
 
 
 
-export default App;
+export default Mainframe;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Mainframe from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Mainframe />
   </React.StrictMode>,
   document.getElementById('root')
 );
